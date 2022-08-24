c553670a3171371c8136a1d9084a2e955dd85c92
Denis Shirokih <abcdenis95@gmail.com>
2022-08-24 17:33:11 +0300

    add items photo

--- src/components/Home/Home/ItemList/ItemList.jsx
+++ src/components/Home/Home/ItemList/ItemList.jsx
@@ -4,13 +4,14 @@ export const ItemList = ({ item }) => {
   return (
     <ItemsWrapper>
       <UlItem>
-        {item.map(({ title, location, description, price, id }) => (
+        {item.map(({ title, location, description, price, id, photo }) => (
           <ItemCard
             key={id}
             title={title}
             location={location}
             description={description}
             price={price}
+            photo={photo}
           />
         ))}
       </UlItem>
