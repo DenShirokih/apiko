12d3698e04be1650e9a44b2971c2173f8a6041e7
Omelchenkormn <omelchenkormn@gmail.com>
2022-09-05 19:19:20 +0300

    add style select language

--- src/components/SelectLanguage/SelectLanguage.jsx
+++ src/components/SelectLanguage/SelectLanguage.jsx
@@ -1,28 +1,52 @@
 import { LOCALES } from 'components/i18n/locales';
 import { LocationContext, HandleChangeContext } from 'components/App';
 import { useContext } from 'react';
+import { BiWorld } from 'react-icons/bi';
+import styled from './select.module.css';
 
 export const SelectLanguage = () => {
-    const currentLocale = useContext(LocationContext)
-    const handleChange = useContext(HandleChangeContext)
+  const currentLocale = useContext(LocationContext);
+  const handleChange = useContext(HandleChangeContext);
+  console.log(styled);
 
   const languages = [
-    { name: 'En', code: LOCALES.ENGLISH },
-    { name: 'Ua', code: LOCALES.UKRANIAN },
+    {
+      name: 'En',
+      code: LOCALES.ENGLISH,
+      flag: 'https://www.svgrepo.com/show/237250/england.svg',
+    },
+    {
+      name: 'Ua',
+      code: LOCALES.UKRANIAN,
+      flag: 'https://www.svgrepo.com/show/237427/ukraine.svg',
+    },
   ];
 
   return (
     <>
-      <div>
-        <select onChange={handleChange} value={currentLocale}>
-          {languages.map(({ name, code }) => (
-          <>
-            <option key={code} value={code}>
-             {name}
-            </option>
+      <div className={styled.dropdown}>
+        <button
+          className={styled.dropbtn}
+          onChange={handleChange}
+          value={currentLocale}
+        >
+          <BiWorld />
+        </button>
+        <div className={styled.dropdownContent}>
+          {languages.map(({ name, code, flag }) => (
+            <>
+              <button
+                className={styled.buttonItem}
+                key={code}
+                value={code}
+                onClick={handleChange}
+              >
+                {name}
+                <img src={flag} alt="" />
+              </button>
             </>
           ))}
-        </select>
+        </div>
       </div>
     </>
   );
